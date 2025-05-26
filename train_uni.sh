f7e09cac19ba003be89488db4c1e6b6d9ad0b3d9
Maryamsana-1998 <msana.bee17seecs@seecs.edu.pk>
2025-05-26 23:19:42 +0900

    full training scripts

--- train_uni.sh
+++ train_uni.sh
@@ -5,11 +5,11 @@
 #SBATCH --mem-per-gpu=29G
 #SBATCH -p batch_grad
 #SBATCH -w ariel-v8
-#SBATCH -o experiments/uni_dir_interpolation/slurm.out
-#SBATCH -e experiments/uni_dir_interpolation/slurm.err
+#SBATCH -o experiments/uni_interpolation/slurm.out
+#SBATCH -e experiments/uni_interpolation/slurm.err
 
 # Set up directories
-EXPERIMENT_DIR="experiments/uni_dir_interpolation"
+EXPERIMENT_DIR="experiments/uni_interpolation"
 LOCAL_CKPT_DIR="${EXPERIMENT_DIR}/local_ckpt"
 LOGS_DIR="${EXPERIMENT_DIR}/logs"
 PRED_DIR="${EXPERIMENT_DIR}/preds"
@@ -19,13 +19,14 @@ mkdir -p ${EXPERIMENT_DIR} ${LOCAL_CKPT_DIR} ${LOGS_DIR}
 # Training parameters
 CONFIG_PATH="configs/uni_interpolation/local_v15.yaml"
 INIT_CKPT="ckpt/uni_dir_interpolation/init_local_uni.ckpt"
-NUM_GPUS=4
-BATCH_SIZE=1
+NUM_GPUS=8
+BATCH_SIZE=2
 NUM_WORKERS=32
-MAX_STEPS=15000
+MAX_STEPS=100000
 
+# lpips + data aug + uni pretrained weights + uni directional decoding 
 
-python src/train/train_sub.py \
+python src/train/train.py \
     --config-path ${CONFIG_PATH} \
     ---resume-path ${INIT_CKPT} \
     ---gpus ${NUM_GPUS} \
